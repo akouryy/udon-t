a59a813afd9d8172ed98e61428f22840140f5659
akouryy <akouryy7@gmail.com>
2015-06-28 12:37:28 +0900

    create TitleStage class

--- src/main/scala/udon/TitleStage.scala
+++ src/main/scala/udon/TitleStage.scala
@@ -1,4 +1,4 @@
-package udon
+package net.akouryy.udon.view
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
@@ -17,49 +17,46 @@ import scalafx.util.Duration
 import java.net.URL
 import javax.sound.sampled._
 
-object ScalaFXHelloWorld extends JFXApp {
+object TitleStage extends PrimaryStage {
+  title = "udon"
 
-  stage = new PrimaryStage {
-    title = "udon"
+  scene = new Scene {
+    fill = Color.web("#F9D1D3")
 
-    scene = new Scene {
-      fill = Color.web("#F9D1D3")
+    content = new HBox {
+      padding = Insets(20)
+      style = "-fx-font-size: 100pt"
 
-      content = new HBox {
-        padding = Insets(20)
-        style = "-fx-font-size: 100pt"
+      effect = new DropShadow {
+        color = Color.web("#fff9f9")
+        radius = 25
+        spread = 0.25
+      }
 
-        effect = new DropShadow {
-          color = Color.web("#fff9f9")
-          radius = 25
-          spread = 0.25
-        }
+      def title_text(c: Char) = new Text {
+        text = c.toString.toUpperCase
+        padding = Insets(10)
 
-        def title_text(c: Char) = new Text {
-          text = c.toString.toUpperCase
-          padding = Insets(10)
+        fill <== when(hover) choose Color.web("#FFFFFF") otherwise Color.web("#753329")
+        stroke <== when(hover) choose Color.web("#753329") otherwise Color.web("#00000000")
+        strokeWidth = 5
 
-          fill <== when(hover) choose Color.web("#FFFFFF") otherwise Color.web("#753329")
-          stroke <== when(hover) choose Color.web("#753329") otherwise Color.web("#00000000")
-          strokeWidth = 5
-
-          onMouseClicked = (ev: MouseEvent) => {
-            val num = ev.button match {
-              case MouseButton.NONE => None
-              case MouseButton.PRIMARY => Some(1)
-              case MouseButton.MIDDLE => None
-              case MouseButton.SECONDARY => Some(2)
-            }
-            num.map { n =>
-              val audioIn = AudioSystem.getAudioInputStream(new java.io.File(s"./resources/op/$c$n.wav"))
-              val clip = AudioSystem.getClip
-              clip.open(audioIn)
-              clip.start
-            }
+        onMouseClicked = (ev: MouseEvent) => {
+          val num = ev.button match {
+            case MouseButton.NONE => None
+            case MouseButton.PRIMARY => Some(1)
+            case MouseButton.MIDDLE => None
+            case MouseButton.SECONDARY => Some(2)
+          }
+          num.map { n =>
+            val audioIn = AudioSystem.getAudioInputStream(new java.io.File(s"./resources/op/$c$n.wav"))
+            val clip = AudioSystem.getClip
+            clip.open(audioIn)
+            clip.start
           }
         }
-        children = "udon".map(title_text _)
       }
+      children = "udon".map(title_text _)
     }
   }
 }
---
+++ src/main/scala/udon/Udon.scala
@@ -0,0 +1,7 @@
+package net.akouryy.udon
+
+import scalafx.application.JFXApp
+
+object Udon extends JFXApp {
+  stage = view.TitleStage
+}
